f7bd56b8fc91f5ee221a13a7d3f9bec89c359e37
zxcvb2002 <jbyjby3743@naver.com>
2023-06-27 23:44:05 +0900

    config: Set Menubar

---
+++ ChatGPTApp/Source/View/HistoryView.swift
@@ -0,0 +1,20 @@
+//
+//  HistoryView.swift
+//  ChatGPTApp
+//
+//  Created by 드즈 on 2023/06/27.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text("HistoryView")
+    }
+}
+
+struct HistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryView()
+    }
+}
